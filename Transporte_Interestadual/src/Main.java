f8f515cab49e9cab2d16aab169225294ea129133
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-03-25 17:19:39 -0300

    Testes

--- Transporte_Interestadual/src/Main.java
+++ Transporte_Interestadual/src/Main.java
@@ -1,23 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         String caminho = "C:\\Users\\bruno\\Desktop\\Teste PUC_DELL";
 
+        // Inicilizar BufferReader e FileReader no try para que sejam desalocados automaticamente no final
+        // sem necessitar de blobo finally
         try (BufferedReader arquivo = new BufferedReader(new FileReader(caminho))) {
-            String linha = arquivo.readLine();
-            String cidades[] = linha.split(",");
-            int i = 0;
+            String linha = arquivo.readLine(); // ler primeira linha
+            String[] cidades = linha.split(","); // capturar nome das cidades na primeira linha
+            int distancias[] = new int[cidades.length];
+            int i = 0; // será usado para contar colunas da matriz
 
             linha = arquivo.readLine();
-            int distancias[] = Integer.parseInt(linha);
+            String[] aux = linha.split(","); // necessário para usar map
+            distancias[] = Arrays.stream(aux).map(Integer::parseInt).forEach();
+
             while (linha != null) {
-                distancias[i++]
             }
         } catch (IOException e) {
             System.out.println("Erro " + e.getMessage());
-        }
+        }fin
     }
 }
