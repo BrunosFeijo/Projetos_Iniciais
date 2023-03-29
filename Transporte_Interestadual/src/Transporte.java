943d834cb3b9761030d52fffe8427b3bcf331bae
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-03-29 05:02:14 -0300

    testes

--- Transporte_Interestadual/src/Transporte.java
+++ Transporte_Interestadual/src/Transporte.java
@@ -2,37 +2,41 @@ import java.util.List;
 
 public class Transporte {
 
-    private Trechos trecho;
-    private int qtdCaminhaoGrande = 0;
-    private int qtdCaminhaoMedio = 0;
-    private int qtdCaminhaoPequeno = 0;
-    //private int distanciaTotal = 0;
+    private final Trechos trecho;
+    private List<Integer> distancias;
+    private List<Double> custos;
+    private int qtdCaminhaoGrande;
+    private int qtdCaminhaoMedio;
+    private int qtdCaminhaoPequeno;
 
 
     public Transporte(Trechos trecho) {
         this.trecho = trecho;
     }
 
-    public double custoTrecho(String cidade1, String cidade2, Modalidades modalidade) {
+    public double custoTrecho(String cidade1, String cidade2, Modalidades modalidade, int qtd) {
 
-        return trecho.distanciaEntreCidades(cidade1, cidade2) * modalidade.getValor();
+        return trecho.distanciaEntreCidades(cidade1, cidade2) * modalidade.getValor() * qtd;
     }
 
     public String modalidadeAdequada(double peso) {
+        qtdCaminhaoGrande = 0; // retorna qtd a zero para refazer o cálculo em caso de entrega de carga parcial
+        qtdCaminhaoMedio = 0;
+        qtdCaminhaoPequeno = 0;
+
         calculoQtdCaminhoes((int) Math.ceil(peso)); // converter peso para inteiro para facilitar o cálculo de caminhões
+
         StringBuilder stringBuilder = new StringBuilder();
         if (qtdCaminhaoGrande > 0) stringBuilder.append(qtdCaminhaoGrande).append(" de Porte Grande - ");
         if (qtdCaminhaoMedio > 0) stringBuilder.append(qtdCaminhaoMedio).append(" de Porte Médio - ");
         if (qtdCaminhaoPequeno > 0) stringBuilder.append(qtdCaminhaoPequeno).append(" de Porte Pequeno - ");
 
-//        qtdCaminhaoGrande = 0; // retorna qtd a zero
-//        qtdCaminhaoMedio = 0;
-//        qtdCaminhaoPequeno = 0;
 
         return stringBuilder.toString();
     }
 
     private int calculoQtdCaminhoes(int peso) {
+        //verifica custo-benefício entre os veículos e as cargas (recursiva)
         if (peso >= Modalidades.GRANDE_PORTE.getPeso()) {
             peso -= Modalidades.GRANDE_PORTE.getPeso();
             qtdCaminhaoGrande++;
@@ -60,18 +64,39 @@ public class Transporte {
 
     public String consultarCustoTrecho(String cidade1, String cidade2, Modalidades modalidade) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("De " + cidade1 + " para " + cidade2 + ", utilizando um " + modalidade.toString() +
-                ", a distância é de " + trecho.distanciaEntreCidades(cidade1, cidade2) + "km" +
-                " e o custo será de R$" + custoTrecho(cidade1, cidade2, modalidade));
+        stringBuilder.append("De ").append(cidade1).append(" para ").append(cidade2)
+                .append(", utilizando um ").append(modalidade.toString())
+                .append(", a distância é de ").append(trecho.distanciaEntreCidades(cidade1, cidade2)).append("km")
+                .append(" e o custo será de R$").append(custoTrecho(cidade1, cidade2, modalidade, 1));
 
         return stringBuilder.toString();
     }
 
     public int distanciaTotalDoTrecho(List<String> listaCidades) {
-        int distanciaTotal= 0;
+        int distanciaTotal = 0;
         for (int i = 0; i < listaCidades.size() - 1; i++) {
-            distanciaTotal += trecho.distanciaEntreCidades(listaCidades.get(i), listaCidades.get(i + 1));
+            distancias.add(trecho.distanciaEntreCidades(listaCidades.get(i), listaCidades.get(i + 1))); // adiciona os trechos separadamente para cálculo final
+            distanciaTotal += distancias.get(distancias.size() - 1);
         }
         return distanciaTotal;
     }
+
+    private void custoFinal(List<String> listaCidades, double peso) {
+        modalidadeAdequada(peso);
+        double custoTotalTrecho = 0;
+        for (int i = 0; i < listaCidades.size() - 1; i++) {
+            if (qtdCaminhaoGrande > 0) {
+                custoTotalTrecho += custoTrecho(listaCidades.get(i), listaCidades.get(i + 1), Modalidades.GRANDE_PORTE, qtdCaminhaoGrande);
+            }
+            if (qtdCaminhaoMedio > 0) {
+                custoTotalTrecho += custoTrecho(listaCidades.get(i), listaCidades.get(i + 1), Modalidades.MEDIO_PORTE, qtdCaminhaoMedio);
+            }
+            if (qtdCaminhaoPequeno > 0) {
+                custoTotalTrecho += custoTrecho(listaCidades.get(i), listaCidades.get(i + 1), Modalidades.PEQUENO_PORTE, qtdCaminhaoPequeno);
+            }
+        }
+
+        custos.add(custoTotalTrecho);
+    }
+
 }
