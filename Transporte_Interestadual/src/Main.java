94d4a97cc64ea588c4703a056b74d249e6a5da59
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-03-28 21:56:41 -0300

    testes

--- Transporte_Interestadual/src/Main.java
+++ Transporte_Interestadual/src/Main.java
@@ -1,12 +1,18 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String caminho = "C:\\Users\\bruno\\Desktop\\Teste PUC_DELL\\DNIT-Distancias.csv";
-        Trechos cidades = null;
+        Trechos cidades = null; // criado fora do try para ser usado posteriormente
+        List<Produtos> produtos = new ArrayList<>(); // permitir que o usuário liste vários itens
+        List<Double> qtdProdutos = new ArrayList<>(); // permitir que o usuário liste suas quantidades
+
         // Inicilizar BufferReader e FileReader no try para que sejam desalocados automaticamente no final
         // sem necessitar de blobo finally
         try (BufferedReader arquivo = new BufferedReader(new FileReader(caminho))) {
@@ -27,10 +33,33 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Erro " + e.getMessage());
         }
-        Transporte t1 = new Transporte(cidades);
+        Transporte t1 = new Transporte(cidades);//inicializa o objeto 'Transporte' com a lista de distancias
 
-        System.out.println(t1.consultarCustoTrecho("Porto Alegre" , "Curitiba", Modalidades.GRANDE_PORTE));
+        System.out.println(t1.consultarCustoTrecho("Manaus" , "Curitiba", Modalidades.GRANDE_PORTE));
         System.out.println(t1.modalidadeAdequada(86000));
 
+    }
+    public static void menuProdutos(){
+        Scanner entrada = new Scanner(System.in);
+        int opcao = -1;
+        Produtos produto;
+        while(opcao != 0){
+            System.out.println("----------Menu de Produtos----------");
+
+            System.out.println("------------------------------------");
+            System.out.println("Escolha um produto: ");
+
+            System.out.println("Escolha a quantidade: ");
+
+        }
+    }
+    public static Produtos definirProduto(int opcao){
+        Produtos produto = null;
+        switch (opcao){
+            
+        }
+
+
+        return produto;
     }
 }
--- Transporte_Interestadual/src/Transporte.java
+++ Transporte_Interestadual/src/Transporte.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Transporte {
 
     private Trechos trecho;
@@ -48,6 +50,25 @@ public class Transporte {
             return calculoQtdCaminhoes(peso);
         }
         return 0;
+    }
+
+    public double calculoPesoProdutos(List<Produtos> produtos, List<Double> qtds) {
+        double peso = 0;
+        int i = 0;
+
+        while(produtos.isEmpty()){
+            switch (produtos.get(i)){
+                case CELULAR -> peso += qtds.get(i) * Produtos.CELULAR.getPeso();
+                case GELADEIRA -> peso += qtds.get(i) * Produtos.GELADEIRA.getPeso();
+                case FREEZER -> peso += qtds.get(i) * Produtos.FREEZER.getPeso();
+                case CADEIRA -> peso += qtds.get(i) * Produtos.CADEIRA.getPeso();
+                case LUMINARIA -> peso += qtds.get(i) * Produtos.LUMINARIA.getPeso();
+                case LAVADORA_DE_ROUPA -> peso += qtds.get(i) * Produtos.LAVADORA_DE_ROUPA.getPeso();
+            }
+            i++;
+        }
+
+        return peso;
     }
 
     public String consultarCustoTrecho(String cidade1, String cidade2, Modalidades modalidade) {
